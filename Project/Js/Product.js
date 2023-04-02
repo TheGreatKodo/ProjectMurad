580364a52b056e0488c5a4cee9183abe22623d55
HoleDestroyer2012 <freddie24w@gmail.com>
2023-04-02 23:41:10 +0300

    js

---
+++ Project/Js/Product.js
@@ -0,0 +1,12 @@
+class Product {
+    constructor(name, png_url, discription, marks, id, price) {
+        this.name = name;
+        this.png_url = png_url;
+        this.discription = discription;
+        this.marks = marks;
+        this.id = id;
+        this.price = price;
+    }
+}
+
+export default Product;
