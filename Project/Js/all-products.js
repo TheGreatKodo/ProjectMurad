0a94fd39ecc405228c735bf75eccb191d9d4dd99
HoleDestroyer2012 <freddie24w@gmail.com>
2023-04-04 20:59:03 +0300

    products

--- Project/Js/all-products.js
+++ Project/Js/all-products.js
@@ -7,8 +7,8 @@ let productCard;
 for (let product of productsDb) {
     productCard = "";
     productCard += `<a href=""><img src="${product.png_url}" class="myImg"></a>`;
-    productCard += `<a href="" class="product-card-link" ><b>${product.price} &#8381</b>`;
-    productCard += `<p>${product.name}</p></a>`;
+    productCard += `<a href="" class="product-card-link" ><p class="product-name">${product.name}</p>`;
+    productCard += `<b class="price">${product.price} &#8381</b></a>`;
     allProducts.push(productCard);
 }
 
@@ -16,16 +16,39 @@ document.querySelectorAll(".product").forEach((element, index) => {
     element.innerHTML = allProducts[index];
     element.setAttribute("data-id", productsDb[index].id);
 
-    element.addEventListener("click", () =>
-        makeCard(element, productsDb, index)
-    );
+    element.addEventListener("click", (e) => {
+        e.preventDefault();
+        makeCard(element, productsDb, index);
+    });
     console.log(element);
 });
 
 let itemCard = "";
-function makeCard(product, productsDb, index) {
+const newProductCard = document.querySelector(".product-card");
+async function makeCard(product, productsDb, index) {
     itemCard = "";
-    itemCard += `<img src="${productsDb[index].png_url}">`;
+    const product_card = document.querySelector(
+        `#product${productsDb[index].id}`
+    );
+    document.querySelector(".main").setAttribute("id", `overlay`);
+    newProductCard.removeAttribute("hidden");
 
-    console.log(itemCard);
+    itemCard += `<p class="close">&times</p>`;
+    itemCard += `<div class="product-contain"><div><img src="${productsDb[index].png_url}"></div>`;
+    itemCard += `<div><p>${productsDb[index].name}</p>`;
+    itemCard += `<p>&#10027&#10027&#10027&#10027&#10027 ${productsDb[index].marks}</p>`;
+    itemCard += `<p>${productsDb[index].price} &#8381</p>`;
+    itemCard += `<p>Описание</p>`;
+    itemCard += `<p>${productsDb[index].discription}</p></div></div>`;
+
+    newProductCard.innerHTML = itemCard;
+
+    const close = document.querySelector(".close");
+    await close.addEventListener("click", () =>{
+        newProductCard.setAttribute("hidden", "")
+        document.querySelector(".main").removeAttribute("id", `overlay`);
+    }
+    );
+
+    console.log(product_card);
 }
